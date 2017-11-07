89e19b8778b75b5563a6cfdc82389ce17bfe4fc2
smithwebtek <brad@smithwebtek.com>
2017-11-07 05:44:24 -0500

    practicing controlled elements and forms

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,41 @@
-// Code ControlledInput Component Here
+import React, { Component } from 'react';
+
+class ControlledInput extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      value: '',
+      happy: true
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleCheckBoxChange = e => {
+    this.setState({
+      happy: e.target.checked
+    });
+  }
+
+  render() {
+    return (
+      <div>
+      <input type="text" onChange={ this.handleChange } value={this.state.name} />
+      <br />
+      <br />
+      <label>
+        Happy?
+        <input type="checkbox" onChange={ this.handleCheckBoxChange } checked={this.state.happy} />
+      </label>
+      <h3>Hello {this.state.name}{(this.state.happy) ? "!" : ":("}</h3>
+      </div>
+    );
+  }
+}
+
+export default ControlledInput; 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlledInput from './components/ControlledInput'
+import NameForm from './components/NameForm'
+import EssayForm from './components/EssayForm'
+import FlavorForm from './components/FlavorForm'
 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
-  document.getElementById('root')
+  <ControlledInput />,
+  document.getElementById('root1')
+);
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root2')
+);
+
+ReactDOM.render(
+  <EssayForm />,
+  document.getElementById('root3')
+);
+
+ReactDOM.render(
+  <FlavorForm />,
+  document.getElementById('root4')
 );
